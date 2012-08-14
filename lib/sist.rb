2eec265674679d55d8d0ea98ba37ccf3ced9a9a1
☈king <rking@sharpsaw.org>
2012-08-13 20:13:54 -0600

    Tests + Docs now 100% YAML-driven.

--- lib/sist.rb
+++ lib/sist.rb
@@ -1,11 +1,21 @@
 class Sist
+  PASSTHRU_ADDRESSES = %w(127.0.0.1 0.0.0.0)
   def self.sanitize input
     result = input.dup
-    result.gsub! /(passw?(or)?d?\s?[=:]\s?)\S+/i do
-      "#$1hiddenpass"
-    end
-    result.gsub! /((?:[ul]\/?n?)[:=])\s?\S+\s?((?:p\/?w?)[:=])\s?\S+/i do
-      "#$1hiddenuser #$2hiddenpass"
+    [
+      [ %r/(passw?(or)?d?\s*[=:]\s*)\S+/i, -> { "#$1hiddenpass" } ],
+      [
+        %r/((?:[ul]\/?n?)[:=])\s*\S+\s*((?:p\/?w?)[:=])\s*\S+/i, -> {
+          "#$1hiddenuser #$2hiddenpass"
+        }
+      ],
+      [
+        %r/((\d+\.){3}\d+)/, -> {
+          if PASSTHRU_ADDRESSES.include? $1; $1 else 'hiddenaddress' end
+        }
+      ],
+    ].each do |xform|
+        result.gsub! xform[0] do xform[1].call end
     end
     result
   end
--- spec/sist_spec.rb
+++ spec/sist_spec.rb
@@ -1,12 +1,42 @@
+# encoding: utf-8
 require 'spec_helper'
+require 'stringio'
+require 'erb'
 
 describe Sist do
-  context 'password filtration' do
-    File.readlines('spec/passwords.in').each do |e|
-      it "should catch #{e}" do
-        Sist.sanitize(e).should_not match(/secr3t/)
-        e.should match(/secr3t/)
+  doc = StringIO.new
+  YAML.load_file('spec/cases.yml').each do |name, intentions|
+    context name do
+      doc.puts '- ' + name
+      intentions.each do |intention|
+        if intention.has_key? 'passthru'
+          intention['passthru'].each do |already_clean|
+            it "should leave '#{already_clean}' alone" do
+              Sist.sanitize(already_clean).should eq(already_clean)
+            end
+            doc.puts "  - `#{already_clean.inspect}` _(unchanged)_"
+          end
+        elsif intention.has_key? 'filter' and intention.has_key? 'from'
+          illicit = intention['filter']
+          intention['from'].each do |dirty|
+            it "should not modify the original" do
+              orig = illicit.dup
+              Sist.sanitize illicit
+              illicit.should eq(orig)
+            end
+            it "should catch #{illicit} from #{dirty}" do
+              Sist.sanitize(dirty).should_not include(illicit)
+            end
+            doc.puts "  - `#{dirty.inspect}` ⇒ " +
+              "`#{Sist.sanitize(dirty).inspect}`"
+          end
+        else
+          fail "Unknown test case: #{intention.to_yaml}"
+        end
       end
     end
   end
+  actual_results = doc.string
+  template = ERB.new File.read('README.md.erb')
+  File.open 'README.md', 'w' do |f| f.write template.result(binding) end
 end
